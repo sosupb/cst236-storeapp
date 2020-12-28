e7f7486ca9b674d98ba35fd1f8af1fabc5a0f630
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-28 15:10:36 -0800

    Fix to database connections.

--- dataService/Database.php
+++ dataService/Database.php
@@ -11,15 +11,21 @@
 class Database
 {
     //properties
-    private $dbServerName = getenv('DB_SERVER'); //"localhost"; //
-    private $dbUsername = getenv('DB_USER_NAME'); //"root"; //
-    private $dbPassword = getenv('DB_PASSWORD'); //"root"; //
-    private $dbName = getenv('DB_NAME'); //"cst236_storeapp_db"; //
-    
+    private $dbServerName;  //"localhost"; //
+    private $dbUsername; //"root"; //
+    private $dbPassword; //"root"; //
+    private $dbName; //"cst236_storeapp_db"; //
+
+    //constructor
+    public function __construct() {
+        $this->dbServerName = getenv('DB_SERVER');
+        $this->dbUsername = getenv('DB_USER_NAME'); 
+        $this->dbPassword = getenv('DB_PASSWORD'); 
+        $this->dbName = getenv('DB_NAME');
+    }
     //methods
     public function getConnection() {
         $conn = new mysqli($this->dbServerName, $this->dbUsername, $this->dbPassword, $this->dbName);
-        
         if($conn->connect_error) {
             echo "Connection failed " .$conn->connect_error . "<br>";
         }
